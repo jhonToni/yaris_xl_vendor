1b52dd0229ded77846fba287b47fb86244d543db
rictec <riktec@gmail.com>
2016-04-08 00:11:49 +0100

    first commit

---
+++ alcatelc7-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yaris_xl/alcatelc7/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yaris_xl/alcatelc7/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yaris_xl/alcatelc7/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yaris_xl/alcatelc7/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/yaris_xl/alcatelc7/proprietary/xbin,system/xbin)
+
